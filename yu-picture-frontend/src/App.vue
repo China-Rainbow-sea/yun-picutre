0164e67a3815a9c32bb0f7608affe75d74d1bb1e
RainbowSea <2969377330@qq.com>
2025-07-16 21:54:14 +0800

    前端Ant Design Vue 默认使用的是英文文案，修改替换为中文(给整个应用包裹一层组件即可完成)

--- yu-picture-frontend/src/App.vue
+++ yu-picture-frontend/src/App.vue
@@ -1,10 +1,17 @@
 
 <template>
   <div id="app">
+    <a-config-provider :locale="zhCN">
+      <BasicLayout />
+    </a-config-provider>
     <BasicLayout />
   </div>
 </template>
 <script setup lang="ts">
+import zhCN from 'ant-design-vue/es/locale/zh_CN';
+import dayjs from 'dayjs';
+import 'dayjs/locale/zh-cn';
+dayjs.locale('zh-cn');
 
 import BasicLayout from '@/layouts/BasicLayout.vue'
 import { useLoginUserStore } from '@/stores/useLoginUserStore'
